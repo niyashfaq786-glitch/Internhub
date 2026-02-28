9cc92d0025539fad7bba79fd0cc3d8da9da09b5d
NIYAS <niyashfaq786@gmail.com>
2026-02-28 20:46:36 +0530

    Delete assets directory

--- assets/js/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-export const SUPABASE_URL = "https://zyssuxxppcqsiwayptwo.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_tmn2azEqCK2CZjFYwFFuHA_RxSvO0DW";
-
-export const supabase = createClient(https://zyssuxxppcqsiwayptwo.supabase.co, sb_publishable_tmn2azEqCK2CZjFYwFFuHA_RxSvO0DW);
