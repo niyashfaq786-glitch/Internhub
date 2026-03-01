a619468e867a59e86074ce6b9f1b3b6cc0a89e13
NIYAS <niyashfaq786@gmail.com>
2026-03-01 08:27:08 +0530

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,6 +0,0 @@
-// assets/js/config.js
-window.INTERNHUB_CONFIG = {
-    SUPABASE_URL: "https://zyssuxxppcqsiwayptwo.supabase.co",
-    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
-
-};
